e76fc1a3d6ce17b365d1ad7b2eda95f0984b75f6
Cory McDonald <cmcdonald@brave.com>
2020-05-16 19:12:53 -0500

    Add React

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,28 @@
 // For webpack
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  ReactDOM.render(
+    <React.Fragment>
+      <h1> Hello from React </h1>
+    </React.Fragment>,
+    document.getElementById("root")
+  );
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 2020_05_16_225540) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
